0b7f51f22ec9eb0c822f7b40b040668c89ca3375
Kai <kaimaruyama511@gmail.com>
2024-09-29 16:34:06 -0400

    Initial configuration

---
+++ public/events.md
@@ -0,0 +1,13 @@
+# // 2.21.5015u
+**IPS-N**'s forces arrive in orbit under the guise of anti-piracy and anti-theft operations.
+
+# // 1.7.5016u
+**COUNCIL HALL** is attacked by unknown organization, codename **FAWKES**. 126 casualties including all council members.
+Martial law is declared.
+**KEYSTONE MASS DRIVER** seized by unknown organization, codename **SABOTEUR**. 
+Used to launch a 25-ton cargo container full of narcotics into the IPS-N flagship **UMV GRAINNE MHAOL**. 
+
+**IPS-N** lands troops to seize the **KEYSTONE MASS DRIVER** and several **ASA INSTALLATIONS**.
+
+# // 2.18.5016u
+DMIL **58TH LOGISTICS COMPANY, FIRST LANCERS** created.
--- src/App.vue
+++ src/App.vue
@@ -95,65 +95,57 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "Archeology",
           "status": "start"
         },
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
-          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "callsign": "UNKNOWN",
+          "alias": "UNKNOWN",
+          "code": "UNKNOWN",
+          "corpro": "UNKNOWN",
+          "frame": "UNKNOWN",
+          "mech": "UNIDENTIFIED MECH"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
-          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "callsign": "UNKNOWN",
+          "alias": "UNKNOWN",
+          "code": "UNKNOWN",
+          "corpro": "UNKNOWN",
+          "frame": "UNKNOWN",
+          "mech": "UNIDENTIFIED MECH"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
-          "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "callsign": "UNKNOWN",
+          "alias": "UNKNOWN",
+          "code": "UNKNOWN",
+          "corpro": "UNKNOWN",
+          "frame": "UNKNOWN",
+          "mech": "UNIDENTIFIED MECH"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
-        },
-        {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Rio Bravo"
+          "callsign": "UNKNOWN",
+          "alias": "UNKNOWN",
+          "code": "UNKNOWN",
+          "corpro": "UNKNOWN",
+          "frame": "UNKNOWN",
+          "mech": "UNIDENTIFIED MECH"
         },
       ],
       "header": {
-        "planet": "Hercynia",
-        "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
+        "planet": "Dustsend",
+        "year": "5016u",
+        "system": "Bejewelled-4",
+        "gate": "Atlas-Djebel Chelia",
         "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
+        "headerTitle": "Dustsend",
+        "headerSubtitle": "58th Logistics",
+        "subheaderTitle": "First Lancers",
         "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
       },
       "options":{
-        "eventsMarkdownPerMission": true
+        "eventsMarkdownPerMission": false
       }
     }
   },
